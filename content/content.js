5f40573ed25ed31630ad48e507ad1113a5a79ef9
Nick Taylor <nick@iamdeveloper.com>
2020-08-12 16:18:20 -0400

    Added aria-label to buttons with no text.

--- content/content.js
+++ content/content.js
@@ -108,14 +108,14 @@ function loadForemHTML(forems) {
     currentOrigin !== 'https://www.forem.com'
   ) {
     actionButton =
-      '<button id="forem-action-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path fill="none" d="M0 0h24v24H0z"/><path d="M11 11V7h2v4h4v2h-4v4h-2v-4H7v-2h4zm1 11C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/></svg></button>';
+      '<button id="forem-action-button" aria-label="Add a Forem instance"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path fill="none" d="M0 0h24v24H0z"/><path d="M11 11V7h2v4h4v2h-4v4h-2v-4H7v-2h4zm1 11C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/></svg></button>';
     script = `<script>
       const event = new CustomEvent("add", { detail: "${currentOrigin}" });
       document.getElementById("forem-action-button").onclick = function() { window.parent.document.dispatchEvent(event) }
     </script>`;
   } else if (currentOrigin !== 'https://www.forem.com') {
     actionButton =
-      '<button id="forem-action-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9.414l2.828-2.829 1.415 1.415L13.414 12l2.829 2.828-1.415 1.415L12 13.414l-2.828 2.829-1.415-1.415L10.586 12 7.757 9.172l1.415-1.415L12 10.586z"/></svg></button>';
+      '<button id="forem-action-button" aria-label="Remove a Forem instance"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9.414l2.828-2.829 1.415 1.415L13.414 12l2.829 2.828-1.415 1.415L12 13.414l-2.828 2.829-1.415-1.415L10.586 12 7.757 9.172l1.415-1.415L12 10.586z"/></svg></button>';
     script = `<script>
       var event = new CustomEvent("remove", { detail: "${currentOrigin}" });
       document.getElementById("forem-action-button").onclick = function() { window.parent.document.dispatchEvent(event) }

2d62246241de7f10fb1797b75ea78b80d6fe78c6
Andrew Bone <AndrewB05@gmail.com>
2020-08-13 19:11:50 +0100

    Use async rather than then (#23)
    * Use async rather than then
    
    * catch up to instant_load

--- content/content.js
+++ content/content.js
@@ -28,36 +28,32 @@ chrome.storage.sync.get(['subscribedForems', 'allforems'], function (result) {
   // Check for new extension version
   if (!allForems || allForems.length === 0 || validOrigins(allForems).includes(currentOrigin)) {
     const init = !allForems || allForems.length === 0;
-    setTimeout(function () {
-      window
-        .fetch('https://www.forem.com/valid_forems.json')
-        .then((response) => {
-          response.json().then((json) => {
-            chrome.storage.sync.set({ allforems: json.forems }); // Create empty array if not initialized.
-            const versionSubstring = json.meta.latestExtensionVersion.substring(
-              0,
-              3,
-            );
-            if (versionSubstring != '0.2') {
-              if (
-                window.confirm(
-                  '👋👋👋\n\nA new beta version of the Forem Browser Extension has been shipped.\n\nDownload the latest from GitHub...',
-                )
-              ) {
-                window.location.href =
-                  'https://github.com/forem/forem-browser-extension';
-              }
-            }
+    setTimeout(async () => {
+      const response = await window.fetch('https://www.forem.com/valid_forems.json');
+      const json = await response.json()
+      chrome.storage.sync.set({ allforems: json.forems }); // Create empty array if not initialized.
+      const versionSubstring = json.meta.latestExtensionVersion.substring(
+        0,
+        3,
+      );
+      if (versionSubstring != '0.2') {
+        if (
+          window.confirm(
+            '👋👋👋\n\nA new beta version of the Forem Browser Extension has been shipped.\n\nDownload the latest from GitHub...',
+          )
+        ) {
+          window.location.href =
+            'https://github.com/forem/forem-browser-extension';
+        }
+      }
 
-            if (init && validOrigins(json.forems).includes(currentOrigin)) {
-              loadForemHTML([]);
-              document.addEventListener('add', handleAdd, false);
-              document.addEventListener('remove', handleRemove, false);
-              document.addEventListener('reorder', handleReorder, false);
-            }
-          });
-        });
-    }, 800);
+      if (init && validOrigins(json.forems).includes(currentOrigin)) {
+        loadForemHTML([]);
+        document.addEventListener('add', handleAdd, false);
+        document.addEventListener('remove', handleRemove, false);
+        document.addEventListener('reorder', handleReorder, false);
+      }
+    }, 800);    
   }
 });
 
